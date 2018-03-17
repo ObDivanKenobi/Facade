aa96448a38e7ca2c88f9683dbc43657ab0c6fb1f
Ilia Manshin <ilia.manshin@yandex.ru>
2018-03-17 14:34:09 +0300

    caesar cifer added

---
+++ Facade/CaesarCryptosystem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    class CaesarCryptosystem
+    {
+        private int _shift;
+
+        public CaesarCryptosystem(int shift)
+        {
+            _shift = shift;
+        }
+
+        public ICollection<int> Encrypt(string message)
+        {
+            List<int> cryptoMessage = new List<int>();
+
+            for (int i = 0; i < message.Length; ++i)
+                cryptoMessage.Add(Encrypt(message[i], _shift));
+
+            return cryptoMessage;
+        }
+
+        public string Decode(ICollection<int> cryptoMessage)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < cryptoMessage.Count; ++i)
+                builder.Append((char)Decrypt(cryptoMessage.ElementAt(i), _shift));
+
+            return builder.ToString();
+        }
+
+        private static int Encrypt(int m, int shift)
+        {
+            return m + shift;
+        }
+
+        private static int Decrypt(int m, int shift)
+        {
+            return m + shift;
+        }
+    }
+}
