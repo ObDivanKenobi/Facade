ab228c634db921b5ba4afe6d8a9e398f6d8f690c
Ilia Manshin <ilia.manshin@yandex.ru>
2018-03-18 12:51:42 +0300

    FAC-2 added static methods and interface prototype.

---
+++ Facade/ElGamalCryptosystem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using CustomMath;
+
+namespace Facade
+{
+    class ElGamalCryptosystem
+    {
+        private readonly int _secretKey;
+        private readonly int _y;
+        private readonly int _g;
+        private readonly int _p;
+        private int _sessionKey;
+
+        public (int y, int g, int p) OpenKey => (_y, _g, _p);
+
+        public ElGamalCryptosystem(int secretKey, int y, int g, int p, int sessionKey)
+        {
+            _secretKey = secretKey;
+            _y = y;
+            _g = g;
+            _p = p;
+            _sessionKey = sessionKey;
+        }
+
+        public ICollection<(int a, int b)> Encrypt(string message, int sessionKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decode(ICollection<(int a, int b)> sipher)
+        {
+            throw new NotImplementedException();
+        }
+
+        static (int a, int b) Encrypt(int q, int k, (int y, int g, int p) openKey)
+        {
+            int a = Calculations.ModPow(openKey.g, k, openKey.p),
+                b = (int)(q * BigInteger.Pow(openKey.y, k) % openKey.p);
+
+            return (a, b);
+        }
+
+        static int Decode(int secretKey, int p, (int a, int b) sipher)
+        {
+            int ax = (int)Math.Pow(sipher.a, secretKey);
+            return sipher.b * Calculations.InvertNotCoprimeIntegers(ax, p) % p;
+        }
+    }
+}
