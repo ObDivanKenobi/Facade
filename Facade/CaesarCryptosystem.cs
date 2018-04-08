37368b4acdbe4564a669a655dbfb5b568ca30c8f
ObDivanKenobi <ilia.manshin@yandex.ru>
2018-04-08 23:54:32 +0300

    FAC-30 1st sonar test

--- Facade/CaesarCryptosystem.cs
+++ Facade/CaesarCryptosystem.cs
@@ -56,7 +56,7 @@ namespace Facade
             int[] array = new[] { 1, 2, 3, 4, 5, 6, 7 };
             foreach (int i in array)
             {
-                int a = i;
+                int a = i+1;
                 int b = i*i*i;
             }
         }
